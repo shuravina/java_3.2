6d2ab69b80c2b2a64c08ff7d71b47479931641d9
Galina Shuravina <shuravinags@mail.ru>
2022-06-18 13:29:39 +0800

    indexerror

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         double height = 1.73; //Рост в метрах
         double weight = 57.4; //Вес в килограммах
         double index = service.calculate(height, weight);
-        System.out.println("Индкс массы тела: " + index);
+        System.out.println("Индекс массы тела: " + index);
 
     }
 }
